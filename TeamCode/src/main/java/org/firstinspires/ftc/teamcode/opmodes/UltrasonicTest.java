648a41f138dd6a2836fd731140083e1bc6451e9e
Charlie Garfield <c22cg@dalton.org>
2022-02-11 15:21:54 -0500

    Add UltrasonicTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/UltrasonicTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.mechanism.MB1242;
+
+public class UltrasonicTest extends OpMode {
+    MB1242 sensor;
+    ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    @Override
+    public void init() {
+        sensor = hardwareMap.get(MB1242.class, "distance1");
+        sensor.ping();
+    }
+
+    @Override
+    public void loop() {
+        if (timer.milliseconds() > 100) {
+            telemetry.addData("Distance", sensor.getDistance(DistanceUnit.INCH));
+            sensor.ping();
+            timer.reset();
+        }
+    }
+}
